b2ab01e3e619b80919f17dff5d1a51f6a56a2c8a
einver <loomgomn@gmail.com>
2026-06-17 19:42:39 +0500

    SmartHome commit2

--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -151,10 +151,18 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
+//}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
