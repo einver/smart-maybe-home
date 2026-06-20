154c959cdbb271c9f5c4229059085e1bde6aa49c
einver <loomgomn@gmail.com>
2026-06-20 16:06:28 +0500

    universal

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5166/api"
-  baseURL: "https://smart-maybe-home.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(config => {
